95db22a6cef167b15e86d5d29544254f7a7462ef
slyCucumber7 <Wesley.micklus@gmail.com>
2025-06-25 23:57:27 -0500

    Added assertJ test import

--- fullstack-bank/backend/web_server/src/test/java/com/github/slycucumber7/web_server/WebServerApplicationTests.java
+++ fullstack-bank/backend/web_server/src/test/java/com/github/slycucumber7/web_server/WebServerApplicationTests.java
@@ -3,11 +3,19 @@ package com.github.slycucumber7.web_server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 @SpringBootTest
 class WebServerApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void exampleTest(){
+        assertThat(1).isEqualTo(2);
     }
 
 }
