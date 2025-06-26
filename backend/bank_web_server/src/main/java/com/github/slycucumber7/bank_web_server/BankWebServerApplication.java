c4130cdeb4906240bc9957f383891060f9a3115b
slyCucumber7 <Wesley.micklus@gmail.com>
2025-06-25 21:17:53 -0500

    Initial commit

---
+++ backend/bank_web_server/src/main/java/com/github/slycucumber7/bank_web_server/BankWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.slycucumber7.bank_web_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWebServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankWebServerApplication.class, args);
+    }
+
+}
