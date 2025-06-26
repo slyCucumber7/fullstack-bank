389cb34cb33ca49053e8b3594dbad733b34ad0e7
slyCucumber7 <Wesley.micklus@gmail.com>
2025-06-25 23:41:54 -0500

    Initial commit

---
+++ fullstack-bank/backend/web_server/src/main/java/com/github/slycucumber7/web_server/WebServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.slycucumber7.web_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServerApplication.class, args);
+    }
+
+}
---
+++ fullstack-bank/backend/web_server/src/test/java/com/github/slycucumber7/web_server/WebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.slycucumber7.web_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
